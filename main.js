efbadfead30e2c4ebd8307a168475f13260c4f7a
PaulQPham <pham.paul.q@gmail.com>
2018-05-16 15:13:19 -0500

    Added API call

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 //import YQL from "yql";
-const apiKey = 'dj0yJmk9b0FHY25zeGs3a1FNJmQ9WVdrOVVtNUNlamhRTnpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1mYw--';
-const url = 'http://query.yahooapis.com/v1/public/yql';
-var query = 'q=select * from weather.forecast where woeid=';
+const API_KEY = 'dj0yJmk9b0FHY25zeGs3a1FNJmQ9WVdrOVVtNUNlamhRTnpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1mYw--';
+const CLIENT_SECRET = '57bebdcd7ee74b83b8beeebd0a574fea0b0c38ba';
+const BASE_URI = 'http://query.yahooapis.com/v1/public/yql';
+const QUERY = 'q=select%20*%20from%20weather.forecast%20where%20woeid=';
+var results = '';
 
 var woeids = [
     2354490,
@@ -20,6 +22,33 @@ var cities = [
     'Los Angeles, CA',
     'Seattle, WA'
 ]
+
+// function YQLQuery(query, callback) {
+//     this.query = query;
+//     this.callback = callback || function(){};
+//     this.fetch = function() {
+
+//         if (!this.query || !this.callback) {
+//             throw new Error('YQLQuery.fetch(): Parameters may be undefined');
+//         }
+
+//         var scriptEl = document.createElement('script'),
+//             uid = 'yql' + +new Date(),
+//             encodedQuery = encodeURIComponent(this.query.toLowerCase()),
+//             instance = this;
+
+//         YQLQuery[uid] = function(json) {
+//             instance.callback(json);
+//             delete YQLQuery[uid];
+//             document.body.removeChild(scriptEl);
+//         };
+
+//         scriptEl.src = 'http://query.yahooapis.com/v1/public/yql?q='
+//                      + encodedQuery + '&format=json&callback=YQLQuery.' + uid; 
+//         document.body.appendChild(scriptEl);
+
+//     };
+// }
 
 function populateListOfCities(citiesArray) {
 
@@ -44,11 +73,22 @@ function populateListOfCities(citiesArray) {
 }
 
 function updateCurrentCity(cityName) {
-    document.getElementById('currentCity').innerHTML = cityName;   
+    document.getElementById('currentCity').innerHTML = cityName;
+
+    const fullQuery = BASE_URI + QUERY + '2354490';
+
+    $.ajax({
+        q: fullQuery,
+        type: 'GET',
+        format: 'json',
+        success(response) {
+            document.getElementById('forecastMessage').innerHTML = query.results.channel.item.forecast;
+        }
+      });
 }
 
 function getCityName(woeid) {
-
+    
 }
 
 document.getElementById('listOfCities').appendChild(populateListOfCities(cities));
