1e5616c0e5509fff5c3a2bebd676094c32444848
PaulQPham <pham.paul.q@gmail.com>
2018-05-17 15:38:11 -0500

    now gets all forecast data

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const API_KEY = 'dj0yJmk9b0FHY25zeGs3a1FNJmQ9WVdrOVVtNUNlamhRTnpZbWNHbzlNQS0tJnM
 const CLIENT_SECRET = '57bebdcd7ee74b83b8beeebd0a574fea0b0c38ba';
 const BASE_URI = 'http://query.yahooapis.com/v1/public/yql/';
 const QUERY = '?q=select%20*%20from%20weather.forecast%20where%20woeid=';
-var results = '';
-var location;
 
 var woeids = [
     2354490,
@@ -43,7 +41,6 @@ function populateListOfCities(citiesArray) {
     item.appendChild(document.createTextNode(citiesArray[i]));
     list.appendChild(item);
   }
-
   return list;
 }
 
@@ -52,29 +49,22 @@ function updateCurrentCity(woeid) {
     const fullQuery = BASE_URI + QUERY + woeid + '&format=json';
     
     $.getJSON(fullQuery, '2354490', function(result) {
-      updateUI(result);
+      updateCurrentCityUI(result);
     });
 }
 
 
-function updateUI(jsonObject) {
+function updateCurrentCityUI(jsonObject) {
     document.getElementById('currentCity').innerHTML = jsonObject.query.results.channel.location.city + ", " + jsonObject.query.results.channel.location.region;
-    var list = document.createElement('ol');
     var index = '0';
     jsonObject.query.results.channel.item.forecast.forEach(function(oneDayForecast) {
-        document.getElementById(index++).appendChild(document.createTextNode(oneDayForecast.date));
+        document.getElementsByClassName('oneDayForecast')[index].innerHTML = '';
+        document.getElementsByClassName('oneDayForecast')[index].appendChild(document.createTextNode(oneDayForecast.day));
+        document.getElementsByClassName('oneDayForecast')[index].appendChild(document.createTextNode(oneDayForecast.date));
+        document.getElementsByClassName('oneDayForecast')[index].appendChild(document.createTextNode(oneDayForecast.high));
+        document.getElementsByClassName('oneDayForecast')[index].appendChild(document.createTextNode(oneDayForecast.low));
+        document.getElementsByClassName('oneDayForecast')[index++].appendChild(document.createTextNode(oneDayForecast.text));
     })
-}
-
-function getCityName(woeid) {
-    
-    const fullQuery = BASE_URI + QUERY + woeid + '&format=json';
-    
-    $.getJSON(fullQuery, '2354490', function(result) {
-      updateUI(result);
-    });
-
-    location = jsonObject.query.results.channel.location.city + ", " + jsonObject.query.results.channel.location.region;
 }
 
 document.getElementById('listOfCities').appendChild(populateListOfCities(cities));
