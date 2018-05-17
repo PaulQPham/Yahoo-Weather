9262e4d0cc58b944e45990cd0bf563e95f76ea52
PaulQPham <pham.paul.q@gmail.com>
2018-05-17 14:16:41 -0500

    api calls now work. initial work on ui for displaying 10 day forecast

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 //import YQL from "yql";
 const API_KEY = 'dj0yJmk9b0FHY25zeGs3a1FNJmQ9WVdrOVVtNUNlamhRTnpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1mYw--';
 const CLIENT_SECRET = '57bebdcd7ee74b83b8beeebd0a574fea0b0c38ba';
-const BASE_URI = 'http://query.yahooapis.com/v1/public/yql';
-const QUERY = 'q=select%20*%20from%20weather.forecast%20where%20woeid=';
+const BASE_URI = 'http://query.yahooapis.com/v1/public/yql/';
+const QUERY = '?q=select%20*%20from%20weather.forecast%20where%20woeid=';
 var results = '';
+var location;
 
 var woeids = [
     2354490,
@@ -23,32 +24,6 @@ var cities = [
     'Seattle, WA'
 ]
 
-// function YQLQuery(query, callback) {
-//     this.query = query;
-//     this.callback = callback || function(){};
-//     this.fetch = function() {
-
-//         if (!this.query || !this.callback) {
-//             throw new Error('YQLQuery.fetch(): Parameters may be undefined');
-//         }
-
-//         var scriptEl = document.createElement('script'),
-//             uid = 'yql' + +new Date(),
-//             encodedQuery = encodeURIComponent(this.query.toLowerCase()),
-//             instance = this;
-
-//         YQLQuery[uid] = function(json) {
-//             instance.callback(json);
-//             delete YQLQuery[uid];
-//             document.body.removeChild(scriptEl);
-//         };
-
-//         scriptEl.src = 'http://query.yahooapis.com/v1/public/yql?q='
-//                      + encodedQuery + '&format=json&callback=YQLQuery.' + uid; 
-//         document.body.appendChild(scriptEl);
-
-//     };
-// }
 
 function populateListOfCities(citiesArray) {
 
@@ -59,10 +34,10 @@ function populateListOfCities(citiesArray) {
     var item = document.createElement('button');
 
     item.id = 'city';
-    item.name = citiesArray[i];
+    item.woeid = woeids[i];
 
     item.addEventListener('click', function() {
-        updateCurrentCity(this.name)
+        updateCurrentCity(this.woeid)
     });
 
     item.appendChild(document.createTextNode(citiesArray[i]));
@@ -72,24 +47,34 @@ function populateListOfCities(citiesArray) {
   return list;
 }
 
-function updateCurrentCity(cityName) {
-    document.getElementById('currentCity').innerHTML = cityName;
+function updateCurrentCity(woeid) {
 
-    const fullQuery = BASE_URI + QUERY + '2354490';
+    const fullQuery = BASE_URI + QUERY + woeid + '&format=json';
+    
+    $.getJSON(fullQuery, '2354490', function(result) {
+      updateUI(result);
+    });
+}
 
-    $.ajax({
-        q: fullQuery,
-        type: 'GET',
-        format: 'json',
-        success(response) {
 
-            document.getElementById('forecastMessage').innerHTML = JSON.parse(response);
-        }
-      });
+function updateUI(jsonObject) {
+    document.getElementById('currentCity').innerHTML = jsonObject.query.results.channel.location.city + ", " + jsonObject.query.results.channel.location.region;
+    var list = document.createElement('ol');
+    var index = '0';
+    jsonObject.query.results.channel.item.forecast.forEach(function(oneDayForecast) {
+        document.getElementById(index++).appendChild(document.createTextNode(oneDayForecast.date));
+    })
 }
 
 function getCityName(woeid) {
     
+    const fullQuery = BASE_URI + QUERY + woeid + '&format=json';
+    
+    $.getJSON(fullQuery, '2354490', function(result) {
+      updateUI(result);
+    });
+
+    location = jsonObject.query.results.channel.location.city + ", " + jsonObject.query.results.channel.location.region;
 }
 
 document.getElementById('listOfCities').appendChild(populateListOfCities(cities));
